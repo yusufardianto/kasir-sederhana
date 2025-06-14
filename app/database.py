a502446657936453627881d416d80f4f9aedde93
fiqifalahdev <fiqifalah17@gmail.com>
2025-06-14 22:26:26 +0700

    change condition

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
+ENV = "development"  # Ganti dengan "production" jika di server
 DATABASE_URL = "mysql+pymysql://root:@localhost:3306/kasir_db"
+DATABASE_URL_PRODUCTION = "mysql+pymysql://root:root@mysql:3306/kasir_db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(ENV == "production" and DATABASE_URL_PRODUCTION or DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
